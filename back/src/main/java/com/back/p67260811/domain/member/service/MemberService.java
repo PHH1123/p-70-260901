3a30d30414e05fb832a6c61c8774f53d4e89b3be
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-09-04 13:06:29 +0900

    61.1

--- back/src/main/java/com/back/p67260811/domain/member/service/MemberService.java
+++ back/src/main/java/com/back/p67260811/domain/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.back.p67260811.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -48,5 +49,13 @@ public class MemberService {
 
     public String genAccessToken(Member member) {
         return authTokenService.genAccessToken(member);
+    }
+
+    public Map<String, Object> payloadOrNull(String accessToken) {
+        return authTokenService.payloadOrNull(accessToken);
+    }
+
+    public Optional<Member> findById(int id) {
+        return memberRepository.findById(id);
     }
 }
--- back/src/main/java/com/back/p67260811/global/rq/Rq.java
+++ back/src/main/java/com/back/p67260811/global/rq/Rq.java
@@ -10,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+
 @Component
 @RequestScope
 @RequiredArgsConstructor
@@ -21,41 +25,56 @@ public class Rq {
     public Member getActor() {
 
         String apiKey = null;
-        String authorization = request.getHeader("Authorization");
+        String accessToken = null;
+        String headerAuthorization = getHeader("Authorization", "");
 
-        if (authorization != null && !authorization.isEmpty()) {
-            if (!authorization.startsWith("Bearer ")) {
-                throw new ServiceException("401-2", "헤더의 인증 정보 형식이 올바르지 않습니다.");
+        if (headerAuthorization.isBlank()) {
+            if (!headerAuthorization.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "Authorization 헤더가 Bearer 형식이 아닙니다.");
             }
 
-            apiKey = authorization.replace("Bearer ", "");
-        }
-        else {
-            Cookie[] cookies = request.getCookies();
+            String[] headerAuthorizationBits = headerAuthorization.split(" ", 3);
 
-            if (cookies == null) {
-                throw new ServiceException("401-1", "인증 정보가 없습니다.");
-            }
-
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("apiKey")) {
-                    apiKey = cookie.getValue();
-                    break;
-                }
-            }
+            apiKey = headerAuthorizationBits[1];
+            accessToken = headerAuthorizationBits.length == 3 ? headerAuthorizationBits[2] : "";
+        } else {
+            apiKey = getCookieValue("apiKey", "");
+            accessToken = getCookieValue("accessToken", "");
         }
 
-        Member actor = memberService.findByApiKey(apiKey)
-                .orElseThrow(() -> new ServiceException("401-3", "API 키가 올바르지 않습니다."));
+        // 아래 원래는 apiKey.isBlank()만 조건인데 둘다 하는 게 맞는 것 같아서 이렇게 수정함
+        if (apiKey.isBlank() && accessToken.isBlank()) {
+            throw new ServiceException("401-1", "로그인 후 이용해주세요.");
+        }
 
-        return actor;
+        Member member = null;
+
+        if (!accessToken.isBlank()) {
+            Map<String, Object> payload = memberService.payloadOrNull(accessToken);
+
+            if (payload != null) {
+                int id = (int) payload.get("id");
+                member = memberService.findById(id)
+                        .orElseThrow(() -> new ServiceException("401-4", "accessToken의 id에 해당하는 회원이 존재하지 않습니다."));
+            }
+        }
+
+        if (member == null) {
+            member = memberService
+                    .findByApiKey(apiKey)
+                    .orElseThrow(() -> new ServiceException("401-3", "API 키가 유효하지 않습니다."));
+        }
+        return member;
     }
 
-    public void addCookie(String name, String value) {
+    public void setCookie(String name, String value) {
         Cookie cookie = new Cookie(name, value);
-        cookie.setDomain("localhost");
         cookie.setPath("/");
         cookie.setHttpOnly(true);
+        if (value.isBlank()) {
+            cookie.setMaxAge(0);
+        }
+
         response.addCookie(cookie);
     }
 
@@ -66,6 +85,27 @@ public class Rq {
         cookie.setHttpOnly(true);
         cookie.setMaxAge(0);
 
-        response.addCookie(cookie);
+        setCookie(name, null);
+    }
+
+    private String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
+    }
+
+    private String getCookieValue(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getCookies())
+                .flatMap(
+                        cookies ->
+                                Arrays.stream(cookies)
+                                        .filter(cookie -> cookie.getName().equals(name))
+                                        .map(Cookie::getValue)
+                                        .filter(value -> !value.isBlank())
+                                        .findFirst()
+                )
+                .orElse(defaultValue);
     }
 }
