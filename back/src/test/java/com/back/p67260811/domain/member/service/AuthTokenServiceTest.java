4f456230d6bc9c26ee4d02ba33a8d7708da5d21e
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-09-04 11:22:35 +0900

    57

--- back/src/test/java/com/back/p67260811/domain/member/service/AuthTokenServiceTest.java
+++ back/src/test/java/com/back/p67260811/domain/member/service/AuthTokenServiceTest.java
@@ -44,27 +44,11 @@ class AuthTokenServiceTest {
     @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=23}")
     void t2() {
 
-        SecretKey secretKey = Keys.hmacShaKeyFor(secretPattern.getBytes(StandardCharsets.UTF_8));
-
-        // 발행 시간과 만료 시간 설정
-        Date issuedAt = new Date();
-        Date expiration = new Date(issuedAt.getTime() + expireMillis);
-
         Map<String, Object> payload = Map.of("name", "Paul", "age", 23);
 
-        String jwt = Jwts.builder()
-                .claims(payload)
-                .issuedAt(issuedAt)
-                .expiration(expiration)
-                .signWith(secretKey)
-                .compact();
+        String jwt = Ut.jwt.toString(secretPattern, expireMillis, payload);
 
-        Map<String, Object> parsedPayload = (Map<String, Object>) Jwts
-                .parser()
-                .verifyWith(secretKey)
-                .build()
-                .parse(jwt)
-                .getPayload();
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwt, secretPattern);
 
         assertThat(parsedPayload)
                 .containsAllEntriesOf(payload);
@@ -77,16 +61,24 @@ class AuthTokenServiceTest {
     @Test
     @DisplayName("Ut.jwt.toString 를 통해서 JWT 생성, {name=\"Paul\", age=23}")
     void t3() {
+
+        Map<String, Object> payload = Map.of("name", "Paul", "age", 23);
+
         String jwt = Ut.jwt.toString(
                 secretPattern,
                 expireMillis,
-                Map.of("name", "Paul", "age", 23)
+                payload
         );
 
         assertThat(jwt).isNotBlank();
 
         boolean validResult = Ut.jwt.isValid(jwt, secretPattern);
         assertThat(validResult).isTrue();
+
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwt, secretPattern);
+
+        assertThat(parsedPayload)
+                .containsAllEntriesOf(payload);
 
         System.out.println("jwt = " + jwt);
     }
