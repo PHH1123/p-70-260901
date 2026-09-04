3550bf5870f659c27c8b62a99535255dc9983a36
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-09-04 10:28:56 +0900

    53

--- back/src/test/java/com/back/p67260811/domain/member/service/AuthTokenServiceTest.java
+++ back/src/test/java/com/back/p67260811/domain/member/service/AuthTokenServiceTest.java
@@ -1,5 +1,6 @@
 package com.back.p67260811.domain.member.service;
 
+import com.back.p67260811.standard.ut.Ut;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +25,10 @@ class AuthTokenServiceTest {
     @Autowired
     private AuthTokenService authTokenService;
 
+    // 토근 만료 기간: 10분
+    private final long expireSeconds = 1000L * 60 * 10;
+    private final String secretPattern= "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+
     @Test
     @DisplayName("authTokenService 서비스가 존재한다.")
     void t1() {
@@ -33,15 +38,12 @@ class AuthTokenServiceTest {
     @Test
     @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=23}")
     void t2() {
-        // 토근 만료 기간: 10분
-        long expireMillis = 1000L * 60 * 10;
 
-        byte[] keyBytes = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes(StandardCharsets.UTF_8);
-        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretPattern.getBytes(StandardCharsets.UTF_8));
 
         // 발행 시간과 만료 시간 설정
         Date issuedAt = new Date();
-        Date expiration = new Date(issuedAt.getTime() + expireMillis);
+        Date expiration = new Date(issuedAt.getTime() + expireSeconds);
 
         String jwt = Jwts.builder()
                 .claims(Map.of("name", "Paul", "age", 23))
@@ -49,6 +51,20 @@ class AuthTokenServiceTest {
                 .expiration(expiration)
                 .signWith(secretKey)
                 .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
+    }
+
+    @Test
+    @DisplayName("Ut.jwt.toString 를 통해서 JWT 생성, {name=\"Paul\", age=23}")
+    void t3() {
+        String jwt = Ut.jwt.toString(
+                secretPattern,
+                expireSeconds,
+                Map.of("name", "Paul", "age", 23)
+        );
 
         assertThat(jwt).isNotBlank();
 
